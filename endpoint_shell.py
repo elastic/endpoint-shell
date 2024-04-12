1f2ab51ce08c6295c90cefc960f97416ee140924
unknown <joseph.desimone@elastic.co>
2024-04-12 11:39:55 -0400

    initial commit

---
+++ endpoint_shell.py
@@ -0,0 +1,114 @@
+import shlex
+import argparse
+import datetime
+
+from prettytable import PrettyTable
+
+import endpoint_api
+import remediate
+
+def shell(session, active_host):
+    agent_id, hostname, os_full = active_host
+    while True:
+        prompt = active_host[1]
+        cmd = input(f"[{active_host[1]}] shell > ")
+        if cmd in ("!exit","exit","!quit","quit"):
+            break
+        if not cmd:
+            continue
+        outputs = session.execute_wait([agent_id], cmd)
+        if not outputs:
+            continue
+        print(outputs.get(agent_id, {}).get("output"))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--kibana-url', help='Kibana URL', required=True)
+    parser.add_argument('--user', dest='user', help='Kibana username', default="elastic")
+    parser.add_argument('--password', dest='password', help='Kibana password')
+    args = parser.parse_args()
+    
+    if not args.password:
+        args.password = input("Enter password: ")
+    
+    session = endpoint_api.Session(args.kibana_url)
+    session.login(args.user, args.password)
+    
+    active_host = None
+    while True:
+        if active_host:
+            prompt = active_host[1]
+        else:
+            prompt = "endpoint shell"
+        cmd = input(f"[{prompt}] > ")
+        
+        if cmd.startswith("!list"):
+            session.display_hosts()
+
+        elif cmd.startswith("!interact"):
+            active_host = None
+            args = cmd.split(" ")
+            if len(args) != 2:
+                print("Usage: !interact <hostname>")
+                continue
+            hosts = session.get_hosts()
+            for host in hosts:
+                agent_id, hostname, os_full = host
+                if args[1] in hostname:
+                    active_host = host
+                    break
+            if not active_host:
+                print("Host not found")
+        
+        elif cmd.startswith("!shell"):
+            if not active_host:
+                print("Use !interact first")
+                continue
+            shell(session, active_host)
+
+        elif cmd.startswith("!download") or cmd.startswith("!dl"):
+            if not active_host:
+                print("Use !interact first")
+                continue
+            args = cmd.split(" ")
+            if len(args) != 2:
+                print("Usage: !download <path>")
+                continue
+            session.get_file_wait(active_host[0], args[1])
+
+        elif cmd.startswith("!upload") or cmd.startswith("!ul"):
+            if not active_host:
+                print("Use !interact first")
+                continue
+            args = shlex.split(cmd, posix=False)
+            if len(args) != 3:
+                print("Usage: !upload <local path> <file name>")
+                continue
+            with open(args[1], "rb") as f:
+                content = f.read()
+            session.upload_file_wait(active_host[0], args[2], content)
+        
+        elif cmd.startswith("!alerts"):
+            start_date = datetime.datetime.utcnow()+datetime.timedelta(hours=24*-1)
+            rows = session.esql_query('from logs-*| where event.kind == "alert" and event.module == "endpoint"|keep process.executable, message, event.id', start_date)
+            if not rows:
+                print("No alerts found")
+                continue
+            pt = PrettyTable(["executable","message","alert id"])
+            for row in rows:
+                pt.add_row(row)
+            pt.align = "l"
+            print(pt)
+
+        elif cmd.startswith("!remediate"):
+            args = cmd.split(" ")
+            if len(args) != 2:
+                print("Usage: !remediate <alert id>")
+                continue
+            remediate.remediate_alert(session, args[1])
+        elif cmd in ("!exit", "exit", "!quit", "quit"):
+            break
+
+if __name__ == "__main__":
+    main()
+
---
+++ remediate.py
@@ -0,0 +1,129 @@
+import os
+import yaml
+import datetime
+import base64
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_PATH = os.path.join(ROOT_DIR, "etc", "remediation_config.yml")
+with open(CONFIG_PATH) as f:
+    CONFIG = yaml.safe_load(f)
+
+def generate_registry_esql(agent_id, user_name):
+    query = f'from logs-*| where event.category == "registry" and agent.id == "{agent_id}" and user.name == "{user_name}"| eval registry.key_lower = to_lower(registry.key) | where '
+    for regkey in CONFIG["registry"]:
+        key = regkey["key"]
+        if key.startswith("HKLM"):
+            hive = "HKLM"
+        else:
+            hive = "HKEY_USERS"
+        key = key.replace("HKLM\\","")
+        key = key.replace("HKEY_USERS\\","")
+        key = key.replace("\\", "\\\\\\\\")
+        key = key.lower()
+        if "values" in regkey:
+            # todo
+            continue
+        else:
+            query += f'( registry.key_lower like "{key}" and registry.hive == "{hive}") or \n'
+        
+    # strip trailing or
+    query = query.rstrip("or \n")
+    query += '| keep registry.hive, registry.key, registry.value, registry.path, registry.data.strings'
+    print(query)
+    
+    return query
+
+def generate_file_esql(agent_id, user_name):
+    query = f'from logs-*| where event.category == "file" and agent.id == "{agent_id}" and user.name == "{user_name}" '
+    query += '| where (file.Ext.header_bytes like "4d5a*") '
+    query += '| keep process.executable, file.path'
+    #print(query)
+    
+    return query
+
+def generate_process_esql(agent_id, user_name):
+    query = f'from logs-*| where event.category == "process" and event.action == "start" and agent.id == "{agent_id}" and user.name == "{user_name}"'
+    query += '| where not (process.code_signature.trusted == true and process.code_signature.subject_name like "*Microsoft*")' # todo or lolbin
+    query += '| keep process.executable, process.entity_id, process.pid'
+    #print(query)
+    
+    return query
+    
+def remediate_alert(session, alert_id):
+    start_date = datetime.datetime.utcnow()+datetime.timedelta(hours=24*-1)
+    rows = session.esql_query(f'from logs-*| where event.kind == "alert" and event.module == "endpoint" and event.id == "{alert_id}"|keep @timestamp, agent.id, user.name', start_date)
+    if not rows:
+        print("Alert not found")
+        return
+    row = rows[0]
+    timestamp, agent_id, user_name = row
+    print(f"Agent id: {agent_id}, User: {user_name}")
+    base_date = datetime.datetime.strptime(timestamp[:26].strip('Z'), "%Y-%m-%dT%H:%M:%S.%f")
+    start_date = base_date+datetime.timedelta(minutes=-5)
+    end_date = base_date+datetime.timedelta(minutes=5)
+    
+    process_terminate_commands = []
+    process_query = generate_process_esql(agent_id, user_name)
+    rows = session.esql_query(process_query, start_date, end_date)
+    if rows:
+        print("Launched processes:")
+        for row in rows:
+            process, entity_id, pid = row
+            print(f"  Process: {process}, Entity: {entity_id}, Pid: {pid}" )
+            cmd = f'Stop-Process -Id {pid} -Force'
+            process_terminate_commands.append(cmd)
+
+    # todo task/service cleanup
+
+    registry_cleanup_commands = []
+    reg_query = generate_registry_esql(agent_id, user_name)
+    rows = session.esql_query(reg_query, start_date, end_date)
+    if rows:
+        print("Registry persistence:")
+        for row in rows:
+            hive, key, value, path, strings = row
+            print(f"  Path: {path}, Data: {strings}" )
+            cmd = f'Remove-ItemProperty -Path "Registry::{hive}\\{key}" -Name "{value}" -Force'
+            registry_cleanup_commands.append(cmd)
+
+    files_to_collect = []
+    file_cleanup_commands = []
+    file_query = generate_file_esql(agent_id, user_name)
+    rows = session.esql_query(file_query, start_date, end_date)
+    if rows:
+        print("Dropped files:")
+        for row in rows:
+            process, file_path = row
+            print(f"  Process: {process}, Path: {file_path}" )
+            files_to_collect.append(file_path)
+            cmd = f'Remove-Item -Path "{file_path}" -Force'
+            file_cleanup_commands.append(cmd)
+
+    r = input("Execute remediation [y/n]: ")
+    if r != "y":
+        return
+    
+    if process_terminate_commands:
+        print("Terminating processes..")
+        cmd = ";".join(process_terminate_commands)
+        cmd_enc = base64.b64encode(cmd.encode('UTF-16LE')).decode()
+        session.execute_wait([agent_id], f"powershell -enc {cmd_enc}")
+
+    if files_to_collect:
+        print("Collecting dropped files..")
+        for file_path in files_to_collect:
+            session.get_file_wait(agent_id, file_path)
+    
+    if file_cleanup_commands or registry_cleanup_commands:
+        print("Cleaning up files/registry..")
+        cmd = ";".join(file_cleanup_commands + registry_cleanup_commands)
+        cmd_enc = base64.b64encode(cmd.encode('UTF-16LE')).decode()
+        session.execute_wait([agent_id], f"powershell -enc {cmd_enc}")
+    
+    print("Remediation complete!")
+
+def main():
+    return
+    
+if __name__ == "__main__":
+    main()
